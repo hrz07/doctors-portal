61a209987360e4054d25158c625a8c01a749ae1f
hrz07 <hridoy.developer@yahoo.com>
2022-05-11 14:31:40 +0600

    treatment hero section added

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import Banner from './Banner';
+import Hero from './Hero';
 import Info from './Info';
+import Services from './Services';
 
 const Home = () => {
     return (
-        <div>
+        <div className='px-12'>
             <Banner></Banner>
             <Info></Info>
+            <Services></Services>
+            <Hero></Hero>
         </div>
     );
 };
