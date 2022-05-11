a5dc594f072d28608f17a981efb2e39de765aaba
hrz07 <hridoy.developer@yahoo.com>
2022-05-11 19:53:18 +0600

    testimonial section done

--- src/pages/Home/Testimonial.js
+++ src/pages/Home/Testimonial.js
@@ -1,7 +1,38 @@
 import React from 'react';
 import quote from '../../assets/icons/quote.svg'
+import people1 from '../../assets/images/people1.png'
+import people2 from '../../assets/images/people2.png'
+import people3 from '../../assets/images/people3.png'
+import Review from './Review'
 
 const Testimonial = () => {
+
+    const reviews = [
+        {
+            _id: 1,
+            name: 'Winson Herry',
+            review: 'It is a long established fact that by the readable content of a lot layout. The point of using Lorem a more-or-less normal distribu to using Content here, content',
+            img : people1,
+            location : 'California'
+        },
+        {
+            _id: 2,
+            name: 'Winson Herry',
+            review: 'It is a long established fact that by the readable content of a lot layout. The point of using Lorem a more-or-less normal distribu to using Content here, content',
+            img : people2,
+            location : 'Texas'
+        },
+        {
+            _id: 3,
+            name: 'Winson Herry',
+            review: 'It is a long established fact that by the readable content of a lot layout. The point of using Lorem a more-or-less normal distribu to using Content here, content',
+            img : people3,
+            location : 'New York'
+        }
+    ]
+
+
+
     return (
         <div className='mt-28'>
             <div className='flex justify-between'>
@@ -13,7 +44,10 @@ const Testimonial = () => {
                     <img src={quote} className='w-24  log:w-48' alt="" />
                 </div>
             </div>
-            <div>
+            <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5'>
+                {
+                    reviews.map(review => <Review key={review._id} review={review} ></Review> )
+                }
             </div>
         </div>
     );
