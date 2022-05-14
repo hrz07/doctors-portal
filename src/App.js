8bd470fd1c7776215ccc529990d8defcb98eaa59
hrz07 <hridoy.developer@yahoo.com>
2022-05-14 09:43:19 +0600

    appointment box added and firebase installed

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@
 import { Route, Routes } from 'react-router-dom';
 import './App.css';
 import About from './pages/About/About';
+import Appointment from './pages/Appointment/Appointment';
 import Home from './pages/Home/Home';
 import Login from './pages/Login/Login';
 import Navbar from './pages/Shared/Navbar';
@@ -14,6 +15,7 @@ function App() {
         <Route path='/' element={ <Home></Home> }></Route>
         <Route path='/about' element={ <About></About> }></Route>
         <Route path='/login' element={ <Login></Login>}></Route>
+        <Route path='/appointment' element={ <Appointment></Appointment> }></Route>
       </Routes>
     </div>
   );
---
+++ src/pages/Appointment/AvailableAppointment.js
@@ -0,0 +1,31 @@
+import { format } from 'date-fns';
+import React, { useEffect, useState } from 'react';
+import BookingModal from './BookingModal';
+import Service from './Service'
+
+const AvailableAppointment = ({ date }) => {
+    
+    const [services, setServices] = useState([])
+    const [treatment,setTreatment] = useState(null)
+    useEffect(() => {
+        fetch('http://localhost:5000/service')
+            .then(res => res.json())
+            .then(data=>setServices(data))
+    },[])
+
+    return (
+        <div>
+            <h1 className='text-xl text-center text-secondary'>Available Appointment on {format(date, 'PP')}</h1>
+            <div className='grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-5 p-10'>
+                {
+                    services.map(service => <Service key={service._id} setTreatment={setTreatment} service={service} ></Service> )
+                }
+            </div>
+            {
+                treatment && <BookingModal date={date} setTreatment={setTreatment} treatment={ treatment} ></BookingModal>
+            } 
+        </div>
+    );
+};
+
+export default AvailableAppointment;
